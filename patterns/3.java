88dd88f352f0782f3465c7c72b578d60f5717fd8
Arunima Chaudhuri <tinniarunima66@gmail.com>
2023-06-13 17:35:16 +0530

    Create 3.java

---
+++ patterns/3.java
@@ -0,0 +1,19 @@
+/*
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+*/
+
+class Solution {
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
