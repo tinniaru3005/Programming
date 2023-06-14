743cd5068621b6037bb3cc36e30f09e5b1eb77c9
Arunima Chaudhuri <tinniarunima66@gmail.com>
2023-06-14 12:02:57 +0530

    Create 15.java

---
+++ patterns/15.java
@@ -0,0 +1,19 @@
+/*
+ABCDE
+ABCD
+ABC
+AB
+A
+*/
+
+class Solution {
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print((char)(j+64));
+            }
+            System.out.println();
+        }
+    }
+}
