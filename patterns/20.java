abae53a294fb9d657f5bf6468cb91a289429cd3e
Arunima Chaudhuri <tinniarunima66@gmail.com>
2023-06-14 12:08:28 +0530

    Create 20.java

---
+++ patterns/20.java
@@ -0,0 +1,48 @@
+/*
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/
+class Solution {
+    void printTriangle(int n) {
+        // code here
+        //2
+        for(int i=1;i<=n;i++){
+            //stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=n-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            //stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //1
+        for(int i=2;i<=n;i++){
+            //stars
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            //space
+            for(int j=2;j<=i;j++){
+                System.out.print("  ");
+            }
+            //stars
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
