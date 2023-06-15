4bc7d86aa10b52e942f449d6261d12f6cd0a6a95
Arunima Chaudhuri <tinniarunima66@gmail.com>
2023-06-15 23:57:29 +0530

    Create 21.java

---
+++ patterns/21.java
@@ -0,0 +1,22 @@
+/*
+****
+*  *
+*  *
+****
+*/
+
+class Solution {
+    void printSquare(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1||j==n||i==1||i==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
